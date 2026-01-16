5ac7b9a66d407a5e2662f8440f2cf56ce19cb769
Koyonari <yongshyan.an@gmail.com>
2026-01-16 15:14:53 +0800

    feat:KILLLLLLLLLLLL

--- components/pointsearned.tsx
+++ components/pointsearned.tsx
@@ -67,15 +67,13 @@ export default function PointsEarned() {
                 <div className="font-poppins text-[11px]">{reward.date}</div>
               </div>
             </div>
-            <div className="border border-[#000000]/30 rounded-3xl p-2 flex flex-row items-center gap-1 max-w-18 min-w-18 h-9">
+            <div className="border border-[#000000]/30 rounded-3xl p-2 flex flex-row items-center gap-1 max-w-20 min-w-20 h-9">
               <img
                 src="points.png"
                 alt="points"
                 className="size-5 object-cover"
               />
-              <div className="font-poppins text-[14px]">
-                + {reward.points}
-              </div>
+              <div className="font-poppins text-[14px]">+ {reward.points}</div>
             </div>
           </div>
         ))}
